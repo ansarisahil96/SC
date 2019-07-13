0d35cd530e7dfc9ae28b86bc3c3c08bfaf38d2e4
Sanjeevan Adhikari <sanjeevan.adhikari@gmail.com>
2019-07-13 21:13:30 +0530

    Login page without the navbar

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,86 @@
+
+<head>
+<title>StudentsCorner Login</title>
+
+<meta charset="utf-8">
+
+
+	 <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+
+	<!-- Custom Theme files -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- //Custom Theme files -->
+	<!-- web font -->
+	<link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+	<!-- //web font -->
+
+</head>
+<body>
+
+<!-- main -->
+<div class="w3layouts-main">
+	<div class="bg-layer">
+		<h1>Login</h1>
+		<div class="header-main">
+			<div class="main-icon">
+				<span class="fa fa-eercast"></span>
+			</div>
+			<div class="header-left-bottom">
+			<form method="POST" action="{{ route('login') }}">
+        @csrf
+					<div class="icon1">
+						<span class="fa fa-user"></span>
+              <input id="email" type="email" placeholder="Email Address" required="" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+              @error('email')
+              <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+              </span>
+          @enderror
+					</div>
+					<div class="icon1">
+						<span class="fa fa-lock"></span>
+            <input id="password" type="password" placeholder="Password" required="" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+          </div>
+					<div class="login-check">
+            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+						 <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>{{ __('Keep me logged in') }}</label>
+					</div>
+					<div class="bottom">
+						<button class="btn">{{ __('Log in') }}</button>
+					</div>
+					<div class="links">
+						<p>  @if (Route::has('password.request'))
+                  <a class="btn btn-link" href="{{ route('password.request') }}">
+                      {{ __('Forgot Your Password?') }}
+                  </a>
+              @endif</p>
+						<p class="right"><a class="nav-link" href="{{ route('register') }}">{{ __('New User? Register') }}</a>
+						<div class="clear"></div>
+					</div>
+				</form>
+			</div>
+			<div class="social">
+				<ul>
+					<li>{{ __('or login using :') }} </li>
+					<li><a href="#" class="facebook"><span class="fa fa-facebook"></span></a></li>
+					<li><a href="#" class="twitter"><span class="fa fa-twitter"></span></a></li>
+					<li><a href="#" class="google"><span class="fa fa-google-plus"></span></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
